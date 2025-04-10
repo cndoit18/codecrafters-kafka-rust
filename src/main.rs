74ea2bbe06a128d6e431c75ac1849dd2d326cf65
cndoit18 <cndoit18@outlook.com>
2025-04-10 13:41:24 +0800

    feat: handle kafka api_version
    Change-Id: Ib402ec354c212b0ac562d74ed661ff409dfdb95c

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,27 @@ fn main() {
 
                 let len = u32::from_be_bytes(buf);
 
+                // 0..2 request_api_key
+                // 2..4 request_api_version
+                // 4..8 correlation_id
                 let mut msg = vec![0_u8; len as usize];
                 stream.read_exact(msg.as_mut_slice()).unwrap();
                 dbg!(&msg);
 
-                // response
                 let mut response = Vec::<u8>::new();
-                response.extend(&[0, 0, 0, 4]);
-                response.extend(&msg[4..8]);
+                let api_version = i16::from_be_bytes([msg[2], msg[3]]);
+                dbg!(&api_version);
+                match api_version {
+                    0..=4 => {
+                        response.extend(&[0, 0, 0, 0]);
+                        response.extend(&msg[4..8]);
+                    }
+                    _ => {
+                        response.extend(&[0, 0, 0, 0]);
+                        response.extend(&msg[4..8]);
+                        response.extend(&[0, 0x23]);
+                    }
+                };
                 stream.write_all(response.as_slice()).unwrap();
             }
             Err(e) => {
