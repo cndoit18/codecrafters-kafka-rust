0b757dd36333c29f220ca32f908d5a60a06c54b0
cndoit18 <cndoit18@outlook.com>
2025-04-12 01:04:53 +0800

    refactor: refactor request parsing logic to use structured Request type

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,11 @@ fn main() {
             Ok(mut stream) => {
                 thread::spawn(move || {
                     println!("accepted new connection");
-                    let mut buf = [0_u8; 4];
-                    while stream.read_exact(&mut buf).is_ok() {
-                        // message_size
-                        dbg!(&buf);
-
-                        let len = u32::from_be_bytes(buf);
-                        // 0..2 request_api_key
-                        // 2..4 request_api_version
-                        // 4..8 correlation_id
-                        let mut msg = vec![0_u8; len as usize];
-                        stream.read_exact(msg.as_mut_slice()).unwrap();
-                        dbg!(&msg);
-
+                    while let Ok(req) = Request::parse(&stream) {
+                        dbg!(&req);
                         let mut message = Vec::<u8>::new();
-                        let api_key = i16::from_be_bytes([msg[0], msg[1]]);
-                        let api_version = i16::from_be_bytes([msg[2], msg[3]]);
-                        dbg!(&api_key, &api_version);
-
-                        if !(1..=4).contains(&api_version) {
-                            message.extend(&msg[4..8]);
+                        if !(1..=4).contains(&req.header.api_version) {
+                            message.extend(req.header.correlation_id.to_be_bytes());
                             message.extend(&[0, 0x23]);
                             let mut response = (message.len() as u32).to_be_bytes().to_vec();
                             response.extend(&message);
@@ -37,11 +22,11 @@ fn main() {
                             return;
                         }
 
-                        match api_key {
+                        match req.header.api_key {
                             // APIVersions
                             18 => {
                                 // correlation id
-                                message.extend(&msg[4..8]);
+                                message.extend(req.header.correlation_id.to_be_bytes());
                                 // error code
                                 message.extend(&[0, 0]);
                                 // num api key records + 1
@@ -85,3 +70,37 @@ fn main() {
         }
     }
 }
+
+#[derive(Default, Debug)]
+struct Header {
+    api_key: i16,
+    api_version: i16,
+    correlation_id: i32,
+    client_id: String,
+}
+
+#[derive(Debug)]
+struct Request {
+    header: Header,
+}
+
+impl Request {
+    fn parse<R: Read>(mut stream: R) -> Result<Request, String> {
+        let mut buf = [0_u8; 4];
+        stream.read_exact(&mut buf).map_err(|err| err.to_string())?;
+        let mut msg = vec![0_u8; u32::from_be_bytes(buf) as usize];
+        stream.read_exact(&mut msg).map_err(|err| err.to_string())?;
+        let mut req = Request {
+            header: Header::default(),
+        };
+        req.header.api_key = i16::from_be_bytes([msg[0], msg[1]]);
+        req.header.api_version = i16::from_be_bytes([msg[2], msg[3]]);
+        req.header.correlation_id = i32::from_be_bytes([msg[4], msg[5], msg[6], msg[7]]);
+        req.header.client_id =
+            String::from_utf8(msg[10..10 + i16::from_be_bytes([msg[8], msg[9]]) as usize].to_vec())
+                .map_err(|err| err.to_string())?;
+
+        Ok(req)
+    }
+}
+enum Message {}
